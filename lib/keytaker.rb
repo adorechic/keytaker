3d58488e1cd3c87fa6aabb0593724311de0fa27e
adorechic <adorechic@gmail.com>
2017-07-06 17:54:22 +0900

    Add CLI via thor

--- lib/keytaker.rb
+++ lib/keytaker.rb
@@ -1,5 +1,14 @@
 require "keytaker/version"
+require "thor"
 
 module Keytaker
-  # Your code goes here...
+  class CLI < Thor
+    desc "take", "Take a key"
+    def take
+    end
+
+    desc "set [key name]", "Set a value"
+    def set(name)
+    end
+  end
 end
