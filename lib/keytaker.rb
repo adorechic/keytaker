26db4eb6cf677d2a07b6617df6938fc8ae27e6cf
adorechic <adorechic@gmail.com>
2017-07-07 11:22:30 +0900

    Extract to model

--- lib/keytaker.rb
+++ lib/keytaker.rb
@@ -6,40 +6,69 @@ require "peco_selector"
 module Keytaker
   SERVICE_NAME = 'ruby-keytaker'
 
+  class KeychainItem
+    class << self
+      def find(key)
+        item = Keychain.generic_passwords.where(service: SERVICE_NAME, account: key).first
+        item ? new(item) : nil
+      end
+
+      def select
+        accounts = Keychain.generic_passwords.where(service: SERVICE_NAME).all.map(&:account)
+        if accounts.empty?
+          nil
+        else
+          key, _ = *PecoSelector.select_from(accounts)
+          KeychainItem.find(key)
+        end
+      end
+
+      def create(key:, value:)
+        Keychain.generic_passwords.create(service: SERVICE_NAME, account: key, password: value)
+      end
+    end
+
+    def initialize(item)
+      @item = item
+    end
+
+    def copy
+      IO.popen('pbcopy', 'w') do |f|
+        f << @item.password
+      end
+    end
+
+    def update(value:)
+      @item.password = value
+      @item.save!
+    end
+
+    def delete
+      @item.delete
+    end
+  end
+
   class CLI < Thor
     desc "take", "Take a key"
     def take
-      accounts = Keychain.generic_passwords.where(service: SERVICE_NAME).all.map(&:account)
-      unless accounts.empty?
-        key, _ = *PecoSelector.select_from(accounts)
-        item = Keychain.generic_passwords.where(service: SERVICE_NAME, account: key).first
-        IO.popen('pbcopy', 'w') do |f|
-          f << item.password
-        end
-      end
+      KeychainItem.select&.copy
     end
 
     desc "set [key name]", "Set a value"
     def set(key)
       value = Readline.readline("value: ")
-      item = Keychain.generic_passwords.where(service: SERVICE_NAME, account: key).first
+      item = KeychainItem.find(key)
 
       if item
-        item.password = value
-        item.save!
+        item.update(value: value)
       else
-        Keychain.generic_passwords.create(service: SERVICE_NAME, account: key, password: value)
+        KeychainItem.create(key: key, value: value)
       end
     end
 
     desc "delete [key name]", "Delete a value"
     def delete
-      accounts = Keychain.generic_passwords.where(service: SERVICE_NAME).all.map(&:account)
-      unless accounts.empty?
-        key, _ = *PecoSelector.select_from(accounts)
-        item = Keychain.generic_passwords.where(service: SERVICE_NAME, account: key).first
-        item.delete
-      end
+      KeychainItem.select&.delete
     end
   end
 end
