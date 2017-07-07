16ca51e6b8bd124ee92944b6bc817ef59246eb95
adorechic <adorechic@gmail.com>
2017-07-07 11:29:33 +0900

    Extract CLI

--- lib/keytaker.rb
+++ lib/keytaker.rb
@@ -1,29 +1,3 @@
 require "keytaker/version"
 require "keytaker/item"
-require "thor"
-
-module Keytaker
-  class CLI < Thor
-    desc "take", "Take a key"
-    def take
-      Keytaker::Item.select&.copy
-    end
-
-    desc "set [key name]", "Set a value"
-    def set(key)
-      value = Readline.readline("value: ")
-      item = Keytaker::Item.find(key)
-
-      if item
-        item.update(value: value)
-      else
-        Keytaker::Item.create(key: key, value: value)
-      end
-    end
-
-    desc "delete [key name]", "Delete a value"
-    def delete
-      Keytaker::Item.select&.delete
-    end
-  end
-end
+require "keytaker/cli"
---
+++ lib/keytaker/cli.rb
@@ -0,0 +1,27 @@
+require "thor"
+
+module Keytaker
+  class CLI < Thor
+    desc "take", "Take a key"
+    def take
+      Keytaker::Item.select&.copy
+    end
+
+    desc "set [key name]", "Set a value"
+    def set(key)
+      value = Readline.readline("value: ")
+      item = Keytaker::Item.find(key)
+
+      if item
+        item.update(value: value)
+      else
+        Keytaker::Item.create(key: key, value: value)
+      end
+    end
+
+    desc "delete [key name]", "Delete a value"
+    def delete
+      Keytaker::Item.select&.delete
+    end
+  end
+end
