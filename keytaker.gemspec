a9335a52ec337e9d099fe53008963b013d31598f
adorechic <adorechic@gmail.com>
2017-07-07 11:07:08 +0900

    Add take, set, delete

--- keytaker.gemspec
+++ keytaker.gemspec
@@ -31,6 +31,8 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'thor'
+  spec.add_dependency 'ruby-keychain'
+  spec.add_dependency 'peco_selector'
 
   spec.add_development_dependency "bundler", "~> 1.14"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/keytaker.rb
+++ lib/keytaker.rb
@@ -1,14 +1,45 @@
 require "keytaker/version"
 require "thor"
+require "keychain"
+require "peco_selector"
 
 module Keytaker
+  SERVICE_NAME = 'ruby-keytaker'
+
   class CLI < Thor
     desc "take", "Take a key"
     def take
+      accounts = Keychain.generic_passwords.where(service: SERVICE_NAME).all.map(&:account)
+      unless accounts.empty?
+        key, _ = *PecoSelector.select_from(accounts)
+        item = Keychain.generic_passwords.where(service: SERVICE_NAME, account: key).first
+        IO.popen('pbcopy', 'w') do |f|
+          f << item.password
+        end
+      end
     end
 
     desc "set [key name]", "Set a value"
-    def set(name)
+    def set(key)
+      value = Readline.readline("value: ")
+      item = Keychain.generic_passwords.where(service: SERVICE_NAME, account: key).first
+
+      if item
+        item.password = value
+        item.save!
+      else
+        Keychain.generic_passwords.create(service: SERVICE_NAME, account: key, password: value)
+      end
+    end
+
+    desc "delete [key name]", "Delete a value"
+    def delete
+      accounts = Keychain.generic_passwords.where(service: SERVICE_NAME).all.map(&:account)
+      unless accounts.empty?
+        key, _ = *PecoSelector.select_from(accounts)
+        item = Keychain.generic_passwords.where(service: SERVICE_NAME, account: key).first
+        item.delete
+      end
     end
   end
 end
